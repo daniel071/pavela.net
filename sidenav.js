26254c02173eef7aa7626eb044958ab55960320a
Daniel Pavela <daniel.pavela@gmail.com>
2020-07-14 19:38:04 +1000

    Add Initial Website
    Signed-off-by: Daniel Pavela <daniel.pavela@gmail.com>

---
+++ sidenav.js
@@ -0,0 +1,9 @@
+/* Open the sidenav */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
+/* Close/hide the sidenav */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
