5e09dc223f411ad4848c264ff10192fc37743a7e
Daniel Pavela <daniel.pavela@gmail.com>
2020-08-02 18:56:09 +1000

    Use locale to update langauge - Add sk locale

--- JavaScript/splashes.js
+++ JavaScript/splashes.js
@@ -1,10 +1,33 @@
-var tip=new Array()
+function getLang() {
+ if (navigator.languages != undefined)
+ return navigator.languages[0];
+ else
+ return navigator.language;
+}
+const addCSS = s =>(d=>{d.head.appendChild(d.createElement("style")).innerHTML=s})(document);
 
-tip[0]="I make games, tools and other software."
-tip[1]="Welcome to my website!"
-tip[2]="Made by Daniel!"
-tip[3]="Refresh the page!"
+var locale = getLang().substring(0, 2)
 
-var random=Math.floor(Math.random()*tip.length)
+var englishTip=new Array()
+var slovakTip=new Array()
 
-document.getElementById("coolSplash").innerHTML = tip[random];
+englishTip[0]="I make games, tools and other software."
+englishTip[1]="Welcome to my website!"
+englishTip[2]="Made by Daniel!"
+englishTip[3]="Refresh the page!"
+
+slovakTip[0]="Vyrábam hry, nástroje a ďalší softvér."
+slovakTip[1]="Vitajte na mojej webovej stránke!"
+slovakTip[2]="Vytvoril Daniel!"
+slovakTip[3]="Obnovte stránku!"
+
+var random=Math.floor(Math.random()*englishTip.length)
+var sheet = window.document.styleSheets[0];
+
+if (locale == "sk") {
+	document.getElementById("coolSplash").innerHTML = slovakTip[random];
+	addCSS(':lang(en) {display:none;}');
+} else {
+	document.getElementById("coolSplash").innerHTML = englishTip[random];
+	addCSS(':lang(sk) {display:none;}');
+}
