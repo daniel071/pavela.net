9445eb84771cad88108748557a6fe27e32a0e2da
Daniel Pavela <daniel.pavela@gmail.com>
2020-08-25 16:17:13 +1000

    Major overhaul with /Daniel/ webpages

--- JavaScript/browserWarning.js
+++ JavaScript/browserWarning.js
@@ -1,4 +1,5 @@
 // Thanks to https://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browser
+// this script is unused at the moment
 
 // Internet Explorer 6-11
 var isIE = /*@cc_on!@*/false || !!document.documentMode;
